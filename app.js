296d2644254ba79e2d76cd9a532c7860b02627d6
maximeroucher <maximeroucher@users.noreply.github.com>
2023-03-02 11:21:50 +0000

    deploy: dd6b194e8f935d1ea9e4d806ee390cfae849ac9b

--- app.js
+++ app.js
@@ -10,6 +10,5 @@ const observer = new IntersectionObserver(callback);
 
 const targets = document.querySelectorAll(".js-show-on-scroll");
 targets.forEach(function (target) {
-  //   target.classList.add("opacity-0");
   observer.observe(target);
 });
